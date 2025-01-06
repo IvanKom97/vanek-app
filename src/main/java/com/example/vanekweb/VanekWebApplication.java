d1881a8c19539d0b6b29d31f3f0f9462c7d4c89d
Иван <dreamscometrueivan97@gmail.com>
2025-01-06 21:40:55 +0200

    start

---
+++ src/main/java/com/example/vanekweb/VanekWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.vanekweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanekWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VanekWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vanekweb/VanekWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vanekweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanekWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
